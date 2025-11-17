e94d02352475b7180c57a84d5e220606e0620090
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2025-11-16 21:20:01 -0800

    Add placeholder for Custom

--- init.el
+++ init.el
@@ -146,3 +146,18 @@
 (setq claude-code-eat-read-only-mode-cursor-type '(bar nil nil))
 
 (keymap-global-set "<f9>" #'claude-code-transient)
+
+;;; Custom
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
